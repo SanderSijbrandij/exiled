cab61a2e4199d74fec21966da7ca0dfef2cc223a
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-07-22 22:50:15 +0200

    add firebase create thingy

--- pages/videos.js
+++ pages/videos.js
@@ -1,9 +1,19 @@
+import React, { Component } from 'react'
 import Layout from '../layouts/default'
 
-export default (props) => {
-  return (
-    <Layout path={props.url.pathname}>
-      <h1>Videos</h1>
-    </Layout>
-  )
+import createVideo from '../lib/create-video'
+
+export default class VideoPage extends Component {
+  createAVideo() {
+    createVideo("hoi", "984789234")
+  }
+
+  render() {
+    return (
+      <Layout path={this.props.url.pathname}>
+        <h1>Videos</h1>
+        <button onClick={() => this.createAVideo()}>create a video :P</button>
+      </Layout>
+    )
+  }
 }
