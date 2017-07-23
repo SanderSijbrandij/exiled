8dc7b0a62b1619c51fdd35683627c09c4c66236a
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-07-23 08:25:33 +0200

    can create a video

--- pages/videos.js
+++ pages/videos.js
@@ -4,15 +4,28 @@ import Layout from '../layouts/default'
 import createVideo from '../lib/create-video'
 
 export default class VideoPage extends Component {
-  createAVideo() {
-    createVideo("hoi", "984789234")
+  async handleSubmit() {
+    const { title, url } = this
+    createVideo(title.value, url.value)
   }
 
   render() {
     return (
       <Layout path={this.props.url.pathname}>
-        <h1>Videos</h1>
-        <button onClick={() => this.createAVideo()}>create a video :P</button>
+        <div className="content-text">
+          <h1>Videos</h1>
+          Working on teh firebase!
+        </div>
+        <div className="content-aside">
+          <h3>add one</h3>
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" ref={c => this.title = c}/>
+          <br />
+          <label htmlFor="url">Youtube ID</label>
+          <input type="text" name="url" ref={c => this.url = c}/>
+          <br />
+          <button onClick={() => this.handleSubmit()}>Create</button>
+        </div>
       </Layout>
     )
   }
