319f9ef62d7e27d2b03389b85453c28863496e8a
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-07-23 22:04:33 +0200

    video reducer

--- reducers/videos.js
+++ reducers/videos.js
@@ -1,5 +1,23 @@
+import types from '../actions/types'
+
 export default (state = [], { type, payload } = {}) => {
   switch(type) {
+    case types.VIDEOS_FETCHED:
+      return [].concat(payload)
+
+    case types.VIDEO_CREATED:
+      return [].concat(payload).concat(state)
+
+    case types.VIDEO_UPDATED:
+      return state.map(video => {
+        return video.id === payload.id ? payload : video
+      })
+
+    case types.VIDEO_DELETED:
+      return state.filter(video => {
+        return video.id !== payload.id
+      })
+
     default:
     return state
   }
